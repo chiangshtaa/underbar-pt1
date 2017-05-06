b74bd6c6a13d630b94e3a74aecc0510746acb456
Michael Chiang <chiangshtaa@gmail.com>
2017-05-05 18:28:12 -0700

    add map test

--- test/map.test.js
+++ test/map.test.js
@@ -6,4 +6,11 @@ describe('map()', () => {
     const mappedArr = _.map(arr, (el) => el * el);
     expect(mappedArr).toEqual([1, 4, 9, 16, 25]);
   });
+
+  it('should return the same array if no callback given', () => {
+    const arr = [1, 2, 3, 4, 5];
+    const mappedArr = _.map(arr);
+    expect(mappedArr).toEqual([1, 2, 3, 4, 5]);
+  });
+  
 });
