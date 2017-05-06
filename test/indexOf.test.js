c6e9d004f64b6c20b5a1f69c4acd43b3fb99e06b
Michael Chiang <chiangshtaa@gmail.com>
2017-05-05 17:12:53 -0700

    add indexOf test

--- test/indexOf.test.js
+++ test/indexOf.test.js
@@ -26,4 +26,9 @@ describe('indexOf()', () => {
     expect(_.indexOf(arr, 'bar', 2)).toBe(3);
   });
 
+  it('returns -1 if starting index > array length -1', () => {
+    const arr = ['foo', 'bar', 'baz', 'bar', 'bar'];
+    expect(_.indexOf(arr, 'foo', 5)).toBe(-1);
+  });
+
 });
