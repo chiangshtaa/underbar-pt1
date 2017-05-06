29714a9ddedbd41db6ba636b644ec9360019b98d
Michael Chiang <chiangshtaa@gmail.com>
2017-05-06 11:42:30 -0700

    add uniq test

--- test/uniq.test.js
+++ test/uniq.test.js
@@ -5,4 +5,10 @@ describe('uniq()', () => {
     const nums = [2, 4, 5, 5, 7, 4, 10, 2];
     expect(_.uniq(nums)).toEqual([2, 4, 5, 7, 10]);
   });
+
+  it('de-dups list of strings, numbers, arrays', () => {
+    const arr = [1, 2, 'hello', 2, 'hi', 'hello', [3, 6], [3, 6]];
+    expect(_.uniq(arr)).toEqual([1, 2, 'hello', 'hi', [3, 6]]);
+  });
+  
 });
